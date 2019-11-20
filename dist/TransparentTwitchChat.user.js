a19526a2cc989ff1acd466aebc8a37031047b201
chylex <contact@chylex.com>
2019-11-20 18:56:25 +0100

    Release 1.4.0

--- dist/TransparentTwitchChat.user.js
+++ dist/TransparentTwitchChat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Transparent Twitch Chat
 // @description  Why decide between missing a PogChamp or sacrificing precious screen space, when you can have the best of both worlds!
-// @version      1.3.5
+// @version      1.4.0
 // @namespace    https://chylex.com
 // @homepageURL  https://github.com/chylex/Transparent-Twitch-Chat
 // @supportURL   https://github.com/chylex/Transparent-Twitch-Chat/issues
@@ -89,81 +89,63 @@ ${isFirefox ? `
 ${rcol} .video-chat__message-list-wrapper:not(.video-chat__message-list-wrapper--unsynced) {overflow-y:hidden!important}
 ` : ``}
 
-${rcolBlur} .channel-root__right-column${wa}, ${rcolBlur} .channel-page__right-column${wa}, ${rcolBlur} .video-watch-page__right-column${wa} {background:rgba(14, 12, 19, ${settings.backgroundOpacity * 0.01})!important}
-${rcol} .channel-root__right-column${wa}, ${rcol} .channel-page__right-column${wa}, ${rcol} .video-watch-page__right-column${wa} {width:${settings.chatWidth - 10}px}
+${rcolBlur} .channel-root__right-column${wa} {background:rgba(14, 12, 19, ${settings.backgroundOpacity * 0.01})!important}
+${rcol}${wa}, ${rcol} .channel-root__right-column${wa} {width:${settings.chatWidth - 10}px!important}
 ${rcol} .video-chat {flex-basis:auto!important}
 ${rcol} .video-chat__header {display:none!important}
-${rcol} .room-selector__header${wa} {border-right:none!important}
 ${rcol} .room-picker {width:${settings.chatWidth - 10}px}
 ${rcol} .hidden {display:none}
 ${rcol} .video-chat__sync-button {width:${settings.chatWidth - 50}px;z-index:10;background-color:#b8b5c0!important}
 
-${settings.chatWidth < 350 ? `
-${rcol} .video-chat__settings, ${rcol} .chat-settings {width:${settings.chatWidth - 50}px}
-` : ``}
+#root[data-a-page-loaded-name="VideoWatchPage"] ${rcolBlur}:not(.right-column--collapsed) .right-column__toggle-visibility {
+display: none !important;
+}
 ${settings.hideHeader ? `
 ${rcolBlur} .rooms-header, ${rcolBlur} .room-selector__header {display:none!important}
+${rcolBlur}:not(.right-column--collapsed) .right-column__toggle-visibility {display:none!important}
 ` : ``}
 ${settings.hideChatInput ? `
-${rcolBlur} .chat-input, ${rcolBlur} .video-chat__input {display:none!important}
+${rcolBlur} .chat-input {display:none!important}
 ` : ``}
 ${settings.hidePinnedCheer ? `
-.pinned-cheer, .pinned-cheer-v2 {display:none}
+.channel-leaderboard {display:none}
 ` : ``}
 ${settings.transparentChat ? `
 body:not(${fullScreen}) .persistent-player--theatre {width:100%!important}
-body:not(${fullWidth}):not(${fullScreen}) .persistent-player--theatre .hover-display div[class|="pl-controls"] {padding-right:${settings.chatWidth - 10}px}
-body:not(${fullScreen}) .persistent-player--theatre .pl-close-button {margin-right:20px}
-body:not(${fullWidth}):not(${fullScreen}) .persistent-player--theatre .pl-close-button {margin-right:${settings.chatWidth + 10}px}
-body:not(${fullScreen}) .persistent-player--theatre .player-streamstatus {margin-right:${settings.chatWidth + 10}px!important;padding-right:1.5em!important}
-body${fullWidth}:not(${fullScreen}) .persistent-player--theatre .player-streamstatus {margin-right:20px!important}
+body:not(${fullWidth}):not(${fullScreen}) .persistent-player--theatre .top-bar,body:not(${fullWidth}):not(${fullScreen}) .persistent-player--theatre .player-controls {padding-right:${settings.chatWidth - 10}px}
+body:not(${fullWidth}):not(${fullScreen}) .persistent-player--theatre .player-overlay-background > div {right:${settings.chatWidth - 10}px!important}
 ${rcol} .chat-room {background:transparent!important}
-${rcol} .tw-border-l.tw-c-background-alt-2${wa} {border-left:none!important}
-${rcolBlur} .video-chat__header {background-color:rgba(23,20,31,0.3984375)!important;box-shadow:none!important}
-${rcolBlur} .video-chat__input {background:transparent!important;box-shadow:none!important}
 ${rcolBlur} .chylex-ttc-chat-container {color:#ece8f3!important}
-${rcol} .room-selector__header {border-left:none!important}
-${rcolBlur} .room-selector__header {background:rgba(14, 12, 19, ${settings.backgroundOpacity * 0.01})!important;border-bottom-color:rgba(44, 37, 65, ${settings.backgroundOpacity * 0.01})!important}
-${rcolBlur} .room-selector__header > p, ${rcolBlur} .room-selector__header .tw-button__text > div {color:#dad8de!important;text-shadow:-1px 0 0 rgba(0,0,0,0.6640625), 0 -1px 0 rgba(0,0,0,0.6640625), 1px 0 0 rgba(0,0,0,0.6640625), 0 1px 0 rgba(0,0,0,0.6640625)}
-${rcolBlur} .chat-input, ${rcolBlur} .video-chat__input {opacity:0.6}
+${rcolBlur} .rooms-header, ${rcolBlur} .leaderboard-header-tabbed-layout {background:transparent!important}
+${rcolBlur} .chat-input {opacity:0.6}
 ${rcolBlur} .chylex-ttc-chat-container {${settings.smoothTextShadow ? `text-shadow:0 0 2px rgba(0,0,0,0.86328125), -1px 0 1px rgba(0,0,0,0.3984375), 0 -1px 1px rgba(0,0,0,0.3984375), 1px 0 1px rgba(0,0,0,0.3984375), 0 1px 1px rgba(0,0,0,0.3984375);` : `text-shadow:-1px 0 0 rgba(0,0,0,0.6640625), 0 -1px 0 rgba(0,0,0,0.6640625), 1px 0 0 rgba(0,0,0,0.6640625), 0 1px 0 rgba(0,0,0,0.6640625);`}}${rcolBlur} .chat-author__display-name, ${rcolBlur} .vod-message__timestamp {${settings.smoothTextShadow ? `text-shadow:-1px 0 1px rgba(0,0,0,0.3984375), 0 -1px 1px rgba(0,0,0,0.3984375), 1px 0 1px rgba(0,0,0,0.3984375), 0 1px 1px rgba(0,0,0,0.3984375);` : `text-shadow:-1px 0 0 rgba(0,0,0,0.53125), 0 -1px 0 rgba(0,0,0,0.53125), 1px 0 0 rgba(0,0,0,0.53125), 0 1px 0 rgba(0,0,0,0.53125);`}}${rcolBlur} .chat-line__message--mention-recipient {text-shadow:none}
-${rcolBlur} .vod-message__timestamp {color:#b7b5ba!important}
 ${rcolBlur} .chat-line__message a {color:#cdb9f5!important}
 .whispers--theatre-mode .whispers-threads-box__container:not(.whispers-threads-box__container--open):not(:hover) {opacity:${Math.max(0.1, settings.backgroundOpacity * 0.01)}}
 ` : `
 body:not(${fullWidth}):not(${fullScreen}) .persistent-player--theatre {width:calc(100% - ${settings.chatWidth - 10}px)!important}
 body:not(${fullScreen}) .persistent-player--theatre .player-streamstatus {margin-right:20px!important}
 `}
-.whispers--theatre-mode.whispers--right-column-expanded {
+.whispers--theatre-mode.whispers--right-column-expanded-beside {
 right: ${settings.chatWidth - 10}px !important;
 }
 ${settings.hideConversations ? `
-.whispers--theatre-mode {display:none}
-.video-player--theatre .video-player__container {bottom:0!important}
-` : ``}
+.whispers--theatre-mode${wa} {display:none!important}
+.highwind-video-player__container--theatre-whispers {bottom:1px!important; // allows hiding player controls in fullscreen by moving cursor all the way down}` : ``}
 ${settings.chatLeftSide && settings.transparentChat ? `
 ${rcol}${wa}, ${rcol} .chat-list__lines .simplebar-track.vertical {left:0!important;right:auto!important}
-body:not(${fullWidth}):not(${fullScreen}) .persistent-player--theatre .hover-display div[class|="pl-controls"] {padding-left:${settings.chatWidth - 10}px;padding-right:0}
-.persistent-player--theatre .pl-close-button {margin-right:0!important}
-body:not(${fullWidth}):not(${fullScreen}) .persistent-player--theatre .player-streaminfo {margin-left:40px}
-body${fullWidth}:not(${fullScreen}) .persistent-player--theatre .player-streaminfo {margin-left:25px}
-body:not(${fullScreen}) .persistent-player--theatre .player-streamstatus${wa} {margin-right:0px!important;padding-right:1.5em!important}
-.whispers--theatre-mode${wa} {right:0px!important}
-${rcol} .right-column__toggle-visibility {left:5px;right:auto;margin-left:${settings.chatWidth - 10}px;transform:rotate(180deg)!important}
-${rcol}.right-column--collapsed .right-column__toggle-visibility {margin-left:0}
+${rcol} .channel-root__right-column${wa} {border-left:none!important;border-right:var(--border-width-default) solid var(--color-border-base)!important}
+body:not(${fullWidth}):not(${fullScreen}) .persistent-player--theatre .top-bar {padding-left:${settings.chatWidth}px;padding-right:0}
+body:not(${fullWidth}):not(${fullScreen}) .persistent-player--theatre .player-controls {padding-left:${settings.chatWidth - 10}px;padding-right:0}
+body:not(${fullWidth}):not(${fullScreen}) .persistent-player--theatre .player-overlay-background > div {left:${settings.chatWidth - 10}px!important;right:0!important}
+.whispers--theatre-mode.whispers--right-column-expanded-beside {right:0px!important}
+${rcol} .right-column__toggle-visibility {transform:rotate(180deg)!important}
+${rcol}.right-column--collapsed .right-column__toggle-visibility {left:0.5rem}
 ` : ``}
 ${settings.grayTheme ? `
-${rcol} .tw-border-l.tw-c-background-alt-2 {border-left-color:#2b2b2b!important}
-${rcol} .channel-root__right-column${wa}, ${rcol} .channel-page__right-column${wa}, ${rcol} .video-watch-page__right-column${wa} {background:#0e0e0e!important}
-${rcolBlur} .channel-root__right-column${wa}, ${rcolBlur} .channel-page__right-column${wa}, ${rcolBlur} .video-watch-page__right-column${wa} {background:rgba(14, 14, 14, ${settings.transparentChat ? (settings.backgroundOpacity * 0.01) : 1})!important}
-${rcolBlur} .room-selector__header${wa} {background:rgba(14, 14, 14, ${settings.transparentChat ? (settings.backgroundOpacity * 0.01) : 1})!important;border-bottom-color:rgba(42, 42, 42, ${settings.transparentChat ? (settings.backgroundOpacity * 0.01) : 1})!important}
-${rcol} .room-selector__header${wa} {background:#0e0e0e!important;border-bottom-color:#2b2b2b!important;${settings.transparentChat ? "" : "border-left-color:#2b2b2b!important;"}}${rcol} .video-chat__input {box-shadow:inset 0 1px 0 0 #2b2b2b!important}
-${rcol} [data-a-target="video-chat-input"], ${rcol} [data-a-target="chat-input"] {background-color:#0e0e0e!important;border-color:#2b2b2b!important}
-${rcol} [data-a-target="video-chat-input"]:focus, ${rcol} [data-a-target="chat-input"]:focus {border-color:#787878!important;box-shadow:0 0 6px -2px #787878!important}
-${rcol} [data-a-target="chat-send-button"], ${rcol} [data-a-target="video-chat-submit-button"] {background-color:#2b2b2b!important;border:1px solid #000000!important}
-${rcol} [data-a-target="chat-send-button"]:active, ${rcol} [data-a-target="chat-send-button"]:focus,${rcol} [data-a-target="video-chat-submit-button"]:active, ${rcol} [data-a-target="video-chat-submit-button"]:focus {box-shadow:0 0 6px 0 #787878!important}
-.whispers--theatre-mode .whispers-threads-box__container:not(.whispers-threads-box__container--open) {border-top-color:#2b2b2b!important;border-right-color:#2b2b2b!important;border-bottom-color:#2b2b2b!important;border-left-color:#2b2b2b!important}
-.whispers--theatre-mode .whispers-threads-box__open-close${wa} {background-color:#0e0e0e!important}
+${rcol} [data-a-target="chat-input"] {background-color:#0e0e0e!important;border-color:#2b2b2b!important}
+${rcol} [data-a-target="chat-input"]:focus {border-color:#787878!important;box-shadow:0 0 6px -2px #787878!important}
+${rcol} [data-a-target="chat-send-button"] {background-color:#2b2b2b!important;border:1px solid #000000!important}
+${rcol} [data-a-target="chat-send-button"]:active, ${rcol} [data-a-target="chat-send-button"]:focus {box-shadow:0 0 6px 0 #787878!important}
 ` : ``}
 
 ${rcolBlur} a[data-a-target="chat-badge"] {opacity:${settings.badgeOpacity / 100};${settings.badgeOpacity === 0 ? `display:none!important;` : ``}}
@@ -191,13 +173,12 @@ function generateSettingsCSS(){
   const style = document.createElement("style");
   style.id = "chylex-ttc-style-settings";
   style.innerHTML = `
-#chylex-ttc-settings-btn {display:none;width:3em;height:3em;position:absolute;bottom:128px;margin-left:290px;z-index:9;cursor:pointer;fill:rgba(255,255,255,0.6640625)}
-.video-chat #chylex-ttc-settings-btn {bottom:135px}
-#chylex-ttc-settings-btn:hover .player-tip {display:inline-block;width:auto;left:50%;top:0.5em;margin-left:-13.75em;z-index:8}
+#chylex-ttc-settings-btn {display:none;width:2.5em;height:2.5em;position:absolute;bottom:105px;margin-left:290px;z-index:9;cursor:pointer;fill:rgba(255,255,255,0.6640625)}
+.video-chat #chylex-ttc-settings-btn {bottom:18px}
 #chylex-ttc-settings-btn svg {width:100%;height:100%}
 #chylex-ttc-settings-btn:hover {fill:#fff}
 .right-column--theatre:hover #chylex-ttc-settings-btn {display:block}
-#chylex-ttc-settings-modal {position:absolute;left:50%;top:50%;width:630px;height:292px;margin-left:-315px;margin-top:-146px;z-index:10000;background-color:rgba(17,17,17,0.796875)}
+#chylex-ttc-settings-modal {position:absolute;left:50%;top:50%;width:860px;height:292px;margin-left:-430px;margin-top:-146px;z-index:10000;background-color:rgba(17,17,17,0.796875)}
 #chylex-ttc-settings-modal #ttc-opt-global-wrapper {position:absolute;margin:5px 0 0 -69px;display:inline-block}
 #chylex-ttc-settings-modal h2 {color:rgba(255,255,255,0.9296875);font-size:24px;text-align:center;margin:0;padding:14px 0 13px;background-color:rgba(0,0,0,0.59765625)}
 #chylex-ttc-settings-modal .ttc-flex-container {display:flex;flex-direction:row;justify-content:space-between;padding:8px 4px}
@@ -213,9 +194,11 @@ function generateSettingsCSS(){
 #chylex-ttc-settings-modal .switch::before, #chylex-ttc-settings-modal .switch::after {width:26px;box-sizing:border-box}
 #chylex-ttc-settings-modal .switch::before {text-align:left;padding-left:5px}
 #chylex-ttc-settings-modal .switch::after {text-align:right;padding-right:3px}
-#chylex-ttc-settings-modal input[type="range"] {width:auto}
-#chylex-ttc-settings-modal .tw-toggle__button::after {border-radius:0}
-#chylex-ttc-settings-modal output {color:rgba(255,255,255,0.796875);width:46px;padding-left:4px;text-align:right}
+#chylex-ttc-settings-modal input[type="text"] {width:100%;padding:2px}
+#chylex-ttc-settings-modal input[type="range"] {width:142px}
+#chylex-ttc-settings-modal .tw-toggle__button {width:4rem}
+#chylex-ttc-settings-modal .tw-toggle__button, #chylex-ttc-settings-modal .tw-toggle__button::after {border-radius:0}
+#chylex-ttc-settings-modal output {color:rgba(255,255,255,0.796875);width:46px;padding-left:4px;text-align:right;vertical-align:40%}
 `;
   
   document.head.appendChild(style);
@@ -306,9 +289,6 @@ var classObserverCallback = function(mutations){
     if (classes.contains("right-column")){
       document.body.classList.toggle("ttc-rcol-collapsed", classes.contains("right-column--collapsed"));
     }
-    else if (classes.contains("video-player")){
-      document.body.classList.toggle("ttc-player-fullscreen", classes.contains("video-player--fullscreen"));
-    }
   }
 };
 
@@ -316,21 +296,30 @@ var classObserver = new MutationObserver(classObserverCallback);
 
 function setupClassHelpers(){
   const col = document.querySelector(".right-column");
-  const player = document.querySelector(".video-player");
   
-  if (!col || !player){
+  if (!col){
     return false;
   }
   
   classObserver.observe(col, { attributes: true, attributeFilter: [ "class" ] });
-  classObserver.observe(player, { attributes: true, attributeFilter: [ "class" ] });
   
   classObserverCallback([
-    { target: col },
-    { target: player }
+    { target: col }
   ]);
   
   return true;
+}
+
+function setupFullscreenHelper(){
+  for(let event of [ "fullscreenchange", "webkitfullscreenchange", "mozfullscreenchange", "msfullscreenchange" ]){
+    if ("on" + event in document){
+      document.addEventListener(event, function(){
+        document.body.classList.toggle("ttc-player-fullscreen", document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement);
+      });
+      
+      break;
+    }
+  }
 }
 
 // Settings
@@ -488,7 +477,7 @@ function insertSettingsButton(){
   
   const button = document.createElement("div");
   button.id = "chylex-ttc-settings-btn";
-  button.innerHTML = '<span class="player-tip js-tip" data-tip="Transparent Twitch Chat"></span><svg><use xlink:href="#icon_settings"></use></svg>';
+  button.innerHTML = '<svg viewBox="0 0 15 15"><path d="M8.463,0.062c-0.639,-0.083 -1.287,-0.083 -1.926,0l-0.251,1.333c-0.802,0.159 -1.565,0.475 -2.244,0.929l-1.12,-0.765c-0.511,0.394 -0.969,0.852 -1.363,1.363l0.765,1.12c-0.454,0.679 -0.77,1.442 -0.929,2.244l-1.333,0.251c-0.083,0.639 -0.083,1.287 0,1.926l1.333,0.251c0.159,0.802 0.475,1.565 0.929,2.244l-0.765,1.12c0.394,0.511 0.852,0.969 1.363,1.363l1.12,-0.765c0.679,0.454 1.442,0.77 2.244,0.929l0.251,1.333c0.639,0.083 1.287,0.083 1.926,0l0.251,-1.333c0.802,-0.159 1.565,-0.475 2.244,-0.929l1.12,0.765c0.511,-0.394 0.969,-0.852 1.363,-1.363l-0.765,-1.12c0.454,-0.679 0.77,-1.442 0.929,-2.244l1.333,-0.251c0.083,-0.639 0.083,-1.287 0,-1.926l-1.333,-0.251c-0.159,-0.802 -0.475,-1.565 -0.929,-2.244l0.765,-1.12c-0.394,-0.511 -0.852,-0.969 -1.363,-1.363l-1.12,0.765c-0.679,-0.454 -1.442,-0.77 -2.244,-0.929l-0.251,-1.333Zm-0.963,2.731c2.598,0 4.707,2.109 4.707,4.707c0,2.598 -2.109,4.707 -4.707,4.707c-2.598,0 -4.707,-2.109 -4.707,-4.707c0,-2.598 2.109,-4.707 4.707,-4.707Z"/><rect x="6.75" y="6" width="1.5" height="5.25"/><rect x="4.89" y="4.5" width="5.221" height="1.5"/></svg>';
   container.appendChild(button);
   
   button.addEventListener("click", function(e){
@@ -553,6 +542,8 @@ window.setInterval(function(){
 document.body.addEventListener("click", function(){
   tryRemoveElement(document.getElementById("chylex-ttc-settings-modal"));
 });
+
+setupFullscreenHelper();
 
 generateSettingsCSS();
 generateCustomCSS();
